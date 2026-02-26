f2679506054ad626c21be32f2f38e5e9922eaf6c
l429609201 <429609201@qq.com>
2026-02-26 22:37:10 +0800

    modified:   web/src/general/Layout.jsx 	modified:   web/src/pages/login/index.jsx

--- web/src/general/Layout.jsx
+++ web/src/general/Layout.jsx
@@ -1,7 +1,7 @@
 import { ErrorBoundary } from 'react-error-boundary'
 import { ErrorFallback } from '../components/ErrorFallback.jsx'
 import { Outlet } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from './Header.jsx'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { isMobileAtom, userinfoAtom } from '../../store/index.js'
@@ -12,6 +12,8 @@ import Cookies from 'js-cookie'
 export const Layout = () => {
   const setUserinfo = useSetAtom(userinfoAtom)
   const isMobile = useAtomValue(isMobileAtom)
+  const [isAuthenticating, setIsAuthenticating] = useState(true)
+
   useEffect(() => {
     const token = Cookies.get('danmu_token')
 
@@ -34,6 +36,9 @@ export const Layout = () => {
         .then(res => {
           if (res && res.data && res.data.username) {
             setUserinfo(res.data)
+            setIsAuthenticating(false)
+          } else {
+            window.location.href = '/login'
           }
         })
         .catch(err => {
@@ -50,6 +55,7 @@ export const Layout = () => {
             window.location.href = '/login'
           } else {
             setUserinfo(res.data)
+            setIsAuthenticating(false)
           }
         })
         .catch(err => {
@@ -62,15 +68,26 @@ export const Layout = () => {
 
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Header />
-      <div
-        className={classNames({
-          'w-full min-h-screen px-4 pb-22 pt-14': isMobile,
-          'max-w-[1200px] min-h-screen mx-auto pt-18 pb-10 px-8': !isMobile,
-        })}
-      >
-        <Outlet />
-      </div>
+      {isAuthenticating ? (
+        // 认证中显示加载状态
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="text-center">
+            <p className="text-base-text text-lg">正在加载...</p>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <div
+            className={classNames({
+              'w-full min-h-screen px-4 pb-22 pt-14': isMobile,
+              'max-w-[1200px] min-h-screen mx-auto pt-18 pb-10 px-8': !isMobile,
+            })}
+          >
+            <Outlet />
+          </div>
+        </>
+      )}
     </ErrorBoundary>
   )
 }
